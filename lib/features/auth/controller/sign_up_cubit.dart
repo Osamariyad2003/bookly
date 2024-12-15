9993c855796a3c3871c95936d619acd668cea8dc
ibraheem.B.J <152438646+ibraheem2210@users.noreply.github.com>
2024-12-15 07:53:38 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lib/main.dart
    #	pubspec.lock

--- lib/features/auth/controller/sign_up_cubit.dart
+++ lib/features/auth/controller/sign_up_cubit.dart
@@ -1,6 +1,30 @@
+//import 'package:bookly/features/auth/controller/sign_up_state.dart';
+import 'package:bookly/features/auth/controller/sign_up_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../core/dio_helper.dart';
 class SignUpCubit extends Cubit{
-  SignUpCubit(super.initialState);
+
+  final DioHelper  _apiService; // Inject your API service
+  SignUpCubit(super.SignUpInitialState, this._apiService);
+
+  Future<void> signUp(String name, String email,String password,String phoneNumber) async {
+    emit(SignUpLoadingState()); // Emit loading state when the sign-in process starts
+
+    try {
+      // Call your API service to attempt signing in
+      final result = await _apiService.signIn(email, password);
+
+      if (result.success == true) {
+        emit(SignInSuccessState()); // Emit success state on successful sign-in
+        _getUser(); // Optionally fetch user data after successful sign-in
+      } else {
+        emit(SignInErrorState()); // Emit error state if sign-in fails
+      }
+
+    } catch (error) {
+      emit(SignInErrorState()); // Emit error state in case of an exception
+    }
+  }
 
 }
